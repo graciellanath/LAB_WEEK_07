21ec2c57871a885c30bd0ddee3a1561d5ff0ea07
graciellanath <graciellanathania8@gmail.com>
2025-10-07 11:50:08 +0700

    Commit No. 2 - Add Google Maps API Key and pinpoint current user location

--- app/src/main/java/com/example/lab_week_07/MapsActivity.kt
+++ app/src/main/java/com/example/lab_week_07/MapsActivity.kt
@@ -2,6 +2,7 @@ package com.example.lab_week_07
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -16,6 +17,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.lab_week_07.databinding.ActivityMapsBinding
+import com.google.android.gms.location.LocationServices
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     //This is the variable through which we will launch the permission request and track user responses
@@ -24,8 +26,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
     private fun getLastLocation() {
-        Log.d("MapsActivity", "getLastLocation() called.")
+        if (hasLocationPermission()) {
+            try {
+                fusedLocationProviderClient.lastLocation
+                    .addOnSuccessListener { location: Location? ->
+                        location?.let {
+                            val userLocation = LatLng(location.latitude,
+                                location.longitude)
+                            updateMapLocation(userLocation)
+                            addMarkerAtLocation(userLocation, "You")
+                        }
+                    }
+            } catch (e: SecurityException) {
+                Log.e("MapsActivity", "SecurityException: ${e.message}")
+            }
+        } else {
+            // If permission was rejected
+            requestPermissionLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
     }
+
+    private fun updateMapLocation(location: LatLng) {
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+            location, 7f))
+    }
+    private fun addMarkerAtLocation(location: LatLng, title: String) {
+        mMap.addMarker(MarkerOptions().title(title)
+            .position(location))
+    }
+
     //This is used to check if the user already has the permission granted
     private fun hasLocationPermission() =
         ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
@@ -42,6 +71,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             .setNegativeButton(android.R.string.cancel) { dialog, _ -> dialog.dismiss()
             }
             .create().show()
+    }
+
+    //A google play location service which helps us interact with Google's Fused Location Provider API
+    //The API intelligently provides us with the device location information
+    private val fusedLocationProviderClient by lazy {
+        LocationServices.getFusedLocationProviderClient(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
